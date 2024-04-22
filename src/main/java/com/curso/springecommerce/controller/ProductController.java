4893d0bd554b0f013939c1641877148c983e4ae1
David1010 <jorge.beltran1.mt@usa.edu.co>
2024-04-22 09:48:09 -0500

    Work in CRUD, create update function, and bug and debug

--- src/main/java/com/curso/springecommerce/controller/ProductController.java
+++ src/main/java/com/curso/springecommerce/controller/ProductController.java
@@ -6,12 +6,14 @@ import com.curso.springecommerce.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 
 @Controller
@@ -50,5 +52,25 @@ public class ProductController {
 
         return "redirect:/product-s";
     }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        Product product= new Product();
+        Optional<Product> optionProduct=productService.get(id);
+        product= optionProduct.get();
+
+        LOGGER.info("Product searched:{}",product);
+        model.addAttribute("product", product);
+
+        return "products/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Product product) {
+        productService.update(product);
+        return "redirect:/product-s";
+    }
+
+
 
 }
